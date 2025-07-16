8183d3810c802b30eeeef43331f91c1afb0dad87
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-07-16 14:55:34 -0600

    Add files via upload

---
+++ Day_1/Numbers.java
@@ -0,0 +1,14 @@
+public class Numbers {
+  public static void main(String[] args){
+      System.out.println("Number");
+      System.out.println(42);
+      System.out.println("Addition");
+      System.out.println(2 + 3);
+      System.out.println("Subtraction");
+      System.out.println(2 - 3);
+      System.out.println("Multiplication");
+      System.out.println(2 * 3);
+      System.out.println("Division");
+      System.out.println(2 / 3.0);
+  }
+}
---
+++ Day_1/Print.java
@@ -0,0 +1,10 @@
+public class Print {
+  public static void main(String[] args){
+      System.out.print("Hello, my name is Peter. ");
+      System.out.println("I am 25 Years old");
+      System.out.print("The result of 5 + 3 is: ");
+      System.out.println(5 + 3);
+      System.out.println("I am leaning\nJava!");
+      System.out.println("Name\tAge\tCountry\nAlice\t25\tUSA\nBob\t30\tUK");
+  }
+}
